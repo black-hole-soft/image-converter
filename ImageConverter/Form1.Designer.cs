0d88eb3d26562693bd010d1809b3313e8578351f
Luis Gerardo Gonzalez Parra <gera.glez@gmail.com>
2020-09-07 10:24:11 -0500

    Add files via upload

---
+++ ImageConverter/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ImageConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 301);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Image Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ImageConverter/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImageConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.openFileDialog1.Multiselect = true;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            this.openFileDialog1.Multiselect = true;
+            foreach (String file in openFileDialog1.FileNames)
+            {
+                MessageBox.Show(file);
+                SaveBmpAsPNG(file);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+        }
+
+        private void SaveBmpAsPNG(String file)
+        {
+            Image img1=Image.FromFile(file);
+            img1.Save(Path.GetDirectoryName(file) + "/" + Path.GetFileNameWithoutExtension(file) + ".png", ImageFormat.Png);
+        }
+    }
+}
